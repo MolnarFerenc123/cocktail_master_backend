d25f24b8484b03a8937110d22a1b23d6b0f65d1f
Molnár Ferenc <nfecoka4@gmail.com>
2026-02-23 11:23:32 +0100

    fix: added port to env file

--- src/data/datasources/db.js
+++ src/data/datasources/db.js
@@ -1,30 +1,27 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env;
 if (!DB_NAME || !DB_USER || !DB_HOST) {
-    console.error("ERROR: Missing database settings from .env file");
-    process.exit(1);
+  console.error("ERROR: Missing database settings from .env file");
+  process.exit(1);
 }
 
-const sequelize = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASS,
-    {
-        host: DB_HOST,
-        dialect: 'mysql',
-        logging: false, 
-        define: {
-            timestamps: false
-        },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }
-);
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "mysql",
+  logging: false,
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 module.exports = sequelize;
